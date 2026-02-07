f8005ecd9c5fe74470ddcb6050bf2e105edf4eaa
Kiara Saahfon <114544229+kiarasaahfon25@users.noreply.github.com>
2026-02-07 17:22:05 -0500

    Added old lab as a starter

---
+++ scripts/groceries.js
@@ -0,0 +1,140 @@
+	
+// Array of products, each product is an object with different fieldset
+// A set of ingredients should be added to products		 
+
+var products = [
+	{
+		name: "brocoli",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 1.99,
+		ingredients: ["broccoli"]
+	},
+	{
+		name: "bread",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: 2.35,
+		ingredients: ["wheat flour", "yeast", "water", "salt"]
+	},
+	{
+		name: "salmon",
+		vegetarian: false,
+		glutenFree: true,
+		organic: false,
+		price: 10.00,
+		ingredients: ["salmon"]
+	},
+	{
+		name: "tofu",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 3.50,
+		ingredients: ["soybeans", "water", "calcium sulfate"]
+	},
+	{
+		name: "chicken breast",
+		vegetarian: false,
+		glutenFree: true,
+		organic: true,
+		price: 7.25,
+		ingredients: ["chicken"]
+	},
+	{
+		name: "pasta",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: 2.99,
+		ingredients: ["wheat semolina", "water"]
+	},
+	{
+		name: "apple",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 0.99,
+		ingredients: ["apple"]
+	},
+	{
+		name: "cheese",
+		vegetarian: true,
+		glutenFree: true,
+		organic: false,
+		price: 4.25,
+		ingredients: ["milk", "salt", "enzymes", "bacterial cultures"]
+	},
+	{
+		name: "milk",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 3.75,
+		ingredients: ["milk"]
+	},
+	{
+		name: "cereal",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: 3.40,
+		ingredients: ["wheat", "sugar", "salt"]
+	}
+];
+
+
+
+// given restrictions provided, make a reduced list of products
+// prices should be included in this list, as well as a sort based on price
+
+function restrictListProducts(products, restrictions) {
+	let product_list = [];
+
+	for (let i = 0; i < products.length; i++) {
+		let product = products[i];
+
+		if (
+			(!restrictions.vegetarian || product.vegetarian) &&
+			(!restrictions.glutenFree || product.glutenFree) &&
+			(!restrictions.organic || product.organic)
+		) {
+			product_list.push({
+				name: product.name,
+				price: product.price
+			});
+		}
+	}
+
+	// Sort by price (low → high)
+	product_list.sort(function (a, b) {
+		return a.price - b.price;
+	});
+
+	return product_list;
+}
+
+
+// Calculate the total price of items, with received parameter being a list of products
+function getTotalPrice(chosenProducts) {
+	totalPrice = 0;
+	for (let i=0; i<products.length; i+=1) {
+		if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
+		}
+	}
+	return totalPrice;
+}
+
+function getPrice(chosenItem){
+	price=0;
+	for (let i in products){
+		if (products[i].name == chosenItem){
+			price = products[i].price
+		}
+	}
+	return price;
+
+}
---
+++ scripts/main.js
@@ -0,0 +1,107 @@
+
+// This function is called when any of the tab is clicked
+// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function openInfo(evt, tabName) {
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+}
+
+
+	
+// generate a checkbox list from a list of products
+// it makes each product name as the label for the checkbos
+
+function populateListProductChoices(slct2) {
+	let s2 = document.getElementById(slct2);
+	s2.innerHTML = "";
+
+	let dietCheckboxes = document.getElementsByName("diet");
+
+	let restrictions = {
+		vegetarian: false,
+		glutenFree: false,
+		organic: false
+	};
+
+	for (let i = 0; i < dietCheckboxes.length; i++) {
+		if (dietCheckboxes[i].checked) {
+			if (dietCheckboxes[i].value === "Vegetarian") restrictions.vegetarian = true;
+			if (dietCheckboxes[i].value === "GlutenFree") restrictions.glutenFree = true;
+			if (dietCheckboxes[i].value === "Organic") restrictions.organic = true;
+			if (dietCheckboxes[i].value === "None") {
+				restrictions.vegetarian = false;
+				restrictions.glutenFree = false;
+				restrictions.organic = false;
+			}
+		}
+	}
+
+	let optionArray = restrictListProducts(products, restrictions);
+
+	for (let i = 0; i < optionArray.length; i++) {
+		let product = optionArray[i];
+
+		let checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = product.name;
+		s2.appendChild(checkbox);
+
+		let label = document.createElement("label");
+		label.appendChild(
+			document.createTextNode(
+				product.name + " ($" + product.price.toFixed(2) + ")"
+			)
+		);
+		s2.appendChild(label);
+
+		s2.appendChild(document.createElement("br"));
+	}
+}
+
+// This function is called when the "Add selected items to cart" button in clicked
+// The purpose is to build the HTML to be displayed (a Paragraph) 
+// We build a paragraph to contain the list of selected items, and the total price
+
+function selectedItems(){
+	
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];
+	
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+	
+	// build list of selected item
+	var para = document.createElement("P");
+	para.innerHTML = "You selected : ";
+	para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) { 
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createTextNode(" -  $" + String(getPrice(ele[i].value))));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+		}
+	}
+		
+	// add paragraph and total price
+	c.appendChild(para);
+	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts)));
+		
+}
