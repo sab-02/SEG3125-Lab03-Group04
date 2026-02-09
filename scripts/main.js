2021a8bd13eada8187ba3017e28ada548d2e55ea
sab-02 <sabirinsaid002@gmail.com>
2026-02-08 20:07:38 -0500

    Added breadcrumbs

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,15 @@
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
 
-function openInfo(evt, tabName) {
 
-	// Get all elements with class="tabcontent" and hide them
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
+function showSection(sectionId) {
+    const sections = document.getElementsByClassName("section");
 
-	// Get all elements with class="tablinks" and remove the class "active"
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].classList.remove("active");
+    }
 
-	// Show the current tab, and add an "active" class to the button that opened the tab
-	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";
-
+    document.getElementById(sectionId).classList.add("active");
 }
 
 
@@ -167,6 +158,6 @@ function updatePriceRange() {
 updatePriceRange();
   
 window.onload = function () {
-    document.getElementById("defaultTab").click();
-	populateShop()
+    showSection("Shop");
+	populateShop();
 };
